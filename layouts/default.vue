3929b3ab850647ee522785a3c7a3882efc722782
百特曼 <289396212@qq.com>
2020-10-29 22:35:02 +0800

    ip docker

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- <NavHeader :active="active" :username="username"></NavHeader> -->
+    <nuxt/>
+  </div>
+</template>
+<script>
+// import NavHeader from '~/components/NavHeader.vue'
+// import NavFooter from '~/components/Footer.vue'
+// import {baseurl, USER_INFO} from '~/plugins/url.js'
+export default {
+  // data() {
+	// 	return {
+	// 		active:'index',
+	// 		username: null
+	// 	}
+  // },
+  // components: {
+	// 	NavHeader,
+	// 	NavFooter
+	// },
+	// mounted(){
+	// 	console.log(JSON.parse(sessionStorage.getItem(USER_INFO)))
+	// 	this.username = JSON.parse(sessionStorage.getItem(USER_INFO)).userName
+	// }
+}
+</script>
+
+
