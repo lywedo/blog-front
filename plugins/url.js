3e17bb71b661df3008ce9022f9afd5a7329e27a3
linyu <linyu@shuwen.com>
2020-12-22 10:07:40 +0800

    https?

--- plugins/url.js
+++ plugins/url.js
@@ -1,4 +1,5 @@
 /*判断环境使用不同接口*/
 // export let baseurl = process.env.NODE_ENV === 'production' ? 'https://api.brianlee.cn' : 'http://localhost:3000';
-export let baseurl = process.env.NODE_ENV === 'production' ? 'http://42.192.36.77:10880' : 'http://42.192.36.77:10880'
+// export let baseurl = process.env.NODE_ENV === 'production' ? 'http://42.192.36.77:10880' : 'http://42.192.36.77:10880'
+export let baseurl = process.env.NODE_ENV === 'production' ? 'http://www.ilovely.xyz:10880' : 'http://www.ilovely.xyz:10880'
 export let USER_INFO = "USER_INFO"
